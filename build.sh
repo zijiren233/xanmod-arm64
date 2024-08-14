1e0f4ba34837e4301ac2fc736b416e4188206d00
zijiren233 <pyh1670605849@gmail.com>
2024-08-14 16:14:49 +0800

    feat: optimization

--- build.sh
+++ build.sh
@@ -51,10 +51,55 @@ else
     cp ../configs/config-6.9.7+bpo-arm64 .config
 fi
 
+undefine() {
+    for _config_name in "$@"; do
+        scripts/config -k --undefine "${_config_name}"
+    done
+}
+
+enable() {
+    for _config_name in "$@"; do
+        scripts/config -k --enable "${_config_name}"
+    done
+}
+
+disable() {
+    for _config_name in "$@"; do
+        scripts/config -k --disable "${_config_name}"
+    done
+}
+
+module() {
+    for _config_name in "$@"; do
+        scripts/config -k --module "${_config_name}"
+    done
+}
+
 scripts/config --set-str CONFIG_LOCALVERSION '-arm64'
 
-scripts/config --disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
-scripts/config --set-val CONFIG_DEBUG_INFO_NONE y
+# debug
+disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
+disable DEBUG_INFO
+enable DEBUG_INFO_NONE
+disable "SLUB_DEBUG" "PM_DEBUG" "PM_ADVANCED_DEBUG" "PM_SLEEP_DEBUG" "ACPI_DEBUG" "SCHED_DEBUG" "LATENCYTOP" "DEBUG_PREEMPT"
+
+# ftrace
+disable "FUNCTION_TRACER" "FUNCTION_GRAPH_TRACER"
+
+# tickless
+disable "NO_HZ_FULL_NODEF" "HZ_PERIODIC" "NO_HZ_FULL" "TICK_CPU_ACCOUNTING" "CONTEXT_TRACKING_FORCE"
+enable "NO_HZ_IDLE" "NO_HZ" "NO_HZ_COMMON" "CONTEXT_TRACKING" "VIRT_CPU_ACCOUNTING" "VIRT_CPU_ACCOUNTING_GEN"
+
+# debian/ubuntu don't properly support zstd module compression
+disable MODULE_COMPRESS_ZSTD
+enable MODULE_COMPRESS_NONE
+
+# cpu gov performance
+disable "CPU_FREQ_DEFAULT_GOV_SCHEDUTIL"
+enable "CPU_FREQ_DEFAULT_GOV_PERFORMANCE" "CPU_FREQ_DEFAULT_GOV_PERFORMANCE_NODEF"
+# cpu gov ondemand
+# disable "CPU_FREQ_DEFAULT_GOV_SCHEDUTIL"
+# enable "CPU_FREQ_DEFAULT_GOV_ONDEMAND" "CPU_FREQ_GOV_ONDEMAND"
 
 # disable sig
 scripts/config --set-str CONFIG_SYSTEM_TRUSTED_KEYS ''
@@ -63,7 +108,9 @@ scripts/config --set-str CONFIG_SYSTEM_REVOCATION_KEYS ''
 scripts/config --disable CONFIG_DEBUG_INFO_BTF # then no need dwarves
 
 # LTO
-scripts/config --enable CONFIG_LTO_CLANG_THIN
+disable LTO_CLANG_FULL
+enable LTO_CLANG_THIN
+disable LTO_NONE
 
 # MODULE SIG SHA1
 scripts/config --set-val CONFIG_MODULE_SIG_SHA1 y
@@ -73,9 +120,16 @@ scripts/config --disable CONFIG_MODULE_SIG_SHA256
 scripts/config --disable CONFIG_MODULE_SIG_SHA384
 scripts/config --disable CONFIG_MODULE_SIG_SHA512
 
-# BBR
-scripts/config --set-val CONFIG_TCP_CONG_BBR y
-scripts/config --set-str CONFIG_DEFAULT_TCP_CONG BBR
+# bbr
+enable "TCP_CONG_ADVANCED"
+_tcp_cong_alg_list=("yeah" "bbr" "cubic" "vegas" "westwood" "reno")
+for _alg in "${_tcp_cong_alg_list[@]}"; do
+    _alg_upper=$(echo "$_alg" | tr '[a-z]' '[A-Z]')
+    enable "TCP_CONG_${_alg_upper}"
+    disable "DEFAULT_${_alg_upper}"
+done
+enable "DEFAULT_BBR"
+scripts/config --set-str "DEFAULT_TCP_CONG" "bbr"
 
 MAKE="make -j$(nproc) ARCH=arm64 LLVM=1 LLVM_IAS=1 KCFLAGS="-pipe""
 
